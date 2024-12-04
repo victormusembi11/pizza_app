1e02ee85c93cabbb01660fb693f9bc34c5a9e4dd
victormusembi11 <victor.musembi11@gmail.com>
2024-12-05 02:21:23 +0300

    feat: Add role parameter to login method in AuthProvider

--- lib/providers/auth_provider.dart
+++ lib/providers/auth_provider.dart
@@ -2,16 +2,20 @@ import 'package:flutter/material.dart';
 
 class AuthProvider extends ChangeNotifier {
   bool _isAuthenticated = false;
+  String? _role;
 
   bool get isAuthenticated => _isAuthenticated;
+  String? get role => _role;
 
-  void login() {
+  void login(String role) {
     _isAuthenticated = true;
+    _role = role;
     notifyListeners();
   }
 
   void logout() {
     _isAuthenticated = false;
+    _role = null;
     notifyListeners();
   }
 }
