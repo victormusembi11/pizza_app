4a6402deb5d083c189979f1c744942132bfc8f8b
victormusembi11 <victor.musembi11@gmail.com>
2024-12-04 09:46:54 +0300

    feat: Add BottomNavBar component
    This commit adds the BottomNavBar component to the project. The BottomNavBar is a StatefulWidget that displays a bottom navigation bar with three tabs: Home, Orders, and Profile. Each tab is associated with a corresponding page: MyHomePage, OrdersPage, and ProfilePage. The BottomNavBar allows the user to navigate between these pages by tapping on the respective tab. Additionally, the BottomNavBar includes a logout button that triggers the logout functionality provided by the AuthProvider. This change improves the user experience by enhancing navigation and providing a consistent UI element across the app.

---
+++ lib/components/bottom_nav_bar.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
+import '../screens/home.dart';
+import '../screens/orders.dart';
+import '../screens/profile.dart';
+
+import '../providers/auth_provider.dart';
+
+class BottomNavBar extends StatefulWidget {
+  const BottomNavBar({super.key});
+
+  @override
+  State<BottomNavBar> createState() => _BottomNavBarState();
+}
+
+class _BottomNavBarState extends State<BottomNavBar> {
+  int _selectedIndex = 0;
+
+  static const List<Widget> _pages = <Widget>[
+    MyHomePage(title: 'Home'),
+    OrdersPage(),
+    ProfilePage(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  void _logout() {
+    context.read<AuthProvider>().logout();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(_selectedIndex == 0
+            ? 'Home'
+            : _selectedIndex == 1
+                ? 'Orders'
+                : 'Profile'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.exit_to_app),
+            onPressed: _logout,
+          ),
+        ],
+      ),
+      body: _pages[_selectedIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart),
+            label: 'Orders',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.green,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
