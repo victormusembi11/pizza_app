32260d009fb236c06035b4f33c23f0adf179e9a6
victormusembi11 <victor.musembi11@gmail.com>
2024-12-05 02:53:15 +0300

    feat: Add API endpoint for retrieving pizzas
    This commit adds a new API endpoint `/api/pizzas/get.php` for retrieving pizzas from the database. The endpoint returns a JSON response containing the list of pizzas. This change enhances the functionality of the application by providing a way to fetch pizza data from the server.

---
+++ api/pizzas/get.php
@@ -0,0 +1,27 @@
+<?php
+
+require_once __DIR__ . '/../connect.php';
+
+header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+    $sql = "SELECT * FROM pizzas";
+    $result = mysqli_query($conn, $sql);
+
+    if (mysqli_num_rows($result) > 0) {
+        $pizzas = [];
+        
+        while ($row = mysqli_fetch_assoc($result)) {
+            $pizzas[] = $row;
+        }
+        
+        echo json_encode(['status' => 'success', 'data' => $pizzas]);
+    } else {
+        echo json_encode(['status' => 'success', 'data' => []]);
+    }
+} else {
+    echo json_encode(['error' => 'Invalid request method. Use GET.']);
+}
