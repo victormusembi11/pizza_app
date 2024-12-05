81e8a96c67675e7112ecbf8bf58885021b951aaf
victormusembi11 <victor.musembi11@gmail.com>
2024-12-05 03:18:23 +0300

    feat: Improve Add New Pizza dialog UI and functionality
    Refactor the Add New Pizza dialog in the AdminPizzasPage component to enhance the user experience and provide a more intuitive interface for adding new pizzas. Update the dialog's title style, add border styling to the input fields, and adjust the button styles for Cancel and Add Pizza. This change improves the usability of the application and makes it easier for users to add pizzas dynamically.

--- lib/screens/admin_pizzas.dart
+++ lib/screens/admin_pizzas.dart
@@ -15,39 +15,52 @@ class AdminPizzasPage extends StatelessWidget {
       context: context,
       builder: (context) {
         return AlertDialog(
-          title: const Text('Add New Pizza'),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          title: const Text('Add New Pizza', style: TextStyle(fontSize: 24)),
           content: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                TextField(
-                  controller: _nameController,
-                  decoration: const InputDecoration(labelText: 'Pizza Name'),
-                ),
-                TextField(
-                  controller: _descriptionController,
-                  decoration: const InputDecoration(labelText: 'Description'),
-                ),
-                TextField(
-                  controller: _priceController,
-                  decoration: const InputDecoration(labelText: 'Price'),
-                  keyboardType: TextInputType.number,
-                ),
-                TextField(
-                  controller: _imageUrlController,
-                  decoration: const InputDecoration(labelText: 'Image URL'),
-                ),
-              ],
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _buildTextField(
+                      _nameController, 'Pizza Name', Icons.local_pizza),
+                  const SizedBox(height: 10),
+                  _buildTextField(
+                      _descriptionController, 'Description', Icons.description),
+                  const SizedBox(height: 10),
+                  _buildTextField(_priceController, 'Price', Icons.attach_money,
+                      keyboardType: TextInputType.number),
+                  const SizedBox(height: 10),
+                  _buildTextField(
+                      _imageUrlController, 'Image URL', Icons.image),
+                ],
+              ),
             ),
           ),
           actions: [
             TextButton(
+              style: TextButton.styleFrom(
+                foregroundColor: Colors.red,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+              ),
               onPressed: () {
                 Navigator.of(context).pop();
               },
-              child: const Text('Cancel'),
+              child: const Text('Cancel', style: TextStyle(fontSize: 16)),
             ),
-            TextButton(
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+              ),
               onPressed: () async {
                 final name = _nameController.text;
                 final description = _descriptionController.text;
@@ -88,11 +101,36 @@ class AdminPizzasPage extends StatelessWidget {
                   );
                 }
               },
-              child: const Text('Add Pizza'),
+              child: const Text('Add Pizza', style: TextStyle(fontSize: 16)),
             ),
           ],
         );
       },
+    );
+  }
+
+  // Helper function to build input fields with icons
+  Widget _buildTextField(
+    TextEditingController controller,
+    String labelText,
+    IconData icon, {
+    TextInputType? keyboardType,
+  }) {
+    return TextFormField(
+      controller: controller,
+      decoration: InputDecoration(
+        labelText: labelText,
+        prefixIcon: Icon(icon, color: Colors.green),
+        filled: true,
+        fillColor: Colors.grey[200],
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+          borderSide: BorderSide.none,
+        ),
+        contentPadding:
+            const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      ),
+      keyboardType: keyboardType,
     );
   }
 
@@ -108,6 +146,7 @@ class AdminPizzasPage extends StatelessWidget {
       floatingActionButton: FloatingActionButton(
         onPressed: () => _addPizza(context),
         tooltip: 'Add Pizza',
+        backgroundColor: Colors.green,
         child: const Icon(Icons.add),
       ),
     );
